eb811f3e28a84964c0089f2eda527740b8858f84
yusufshhzd <yusufshhzd@gmail.com>
2025-05-23 20:07:28 -0400

    home

--- src/pages/Vocab/Home.tsx
+++ src/pages/Vocab/Home.tsx
@@ -6,9 +6,21 @@ import { AppSidebar } from '@/components/app-sidebar';
 import { Paper, Typography } from '@mui/material';
 import ConjugationTable from '@/components/ConjugationTable';
 import ProgressBar from '@/components/ProgressBar/ProgressBar';
+import { FlashcardArray } from 'react-quizlet-flashcard';
 
 
 const Home: React.FC = () => {
+
+    const Nouns = [['English', 'Urdu'], ['House/Home', 'Ghar'], ['Room', 'Kamra'], ['Kitchen', 'Bawarchi Khana'],
+    ['Bed', 'Bistar'], ['Chair', 'Kursi'], ['Table', 'Mez'], ['Door', 'Darwaza'],
+    ['Window', 'Khirki'], ['Fan', 'Pankha'], ['Wall', 'Deewar'], ['Floor', 'Farsh'], ['Roof', 'Chhat'],]
+
+    const Verbs = [['English', 'Urdu'], ['To sleep', 'Sona'], ['Jaagna', 'To wake up'], ['Baithna', 'To sit'],
+    ['Khana', 'To Eat'], ['Peena', 'To drink'], ['Saaf karna', 'To clean'], ['Band karna', 'To close'], ['Kholna', 'To open'],
+    ['Jalana', 'To turn on'], ['Bujhana', 'To turn off'], ['Rakhna', 'To put/place'], ['Nikaalna', 'To take out']]
+
+    const Adjectives = [['English', 'Urdu'], ['Saaf', 'Clean'], ['Ganda', 'Dirty'], ['Bara', 'Big'], ['Chhota', 'Small']]
+
 
     return (
         <div>
@@ -18,12 +30,36 @@ const Home: React.FC = () => {
                 <SidebarTrigger />
                 <div className="rightOfSidebar">
                     <ProgressBar />
-                  
+
                     <Typography variant="h4">Home (Ghar)</Typography>
-                   
+
                     <Paper elevation={7} sx={{ backgroundColor: "#E8E9EB", width: 3 / 4, marginTop: '50px' }} >
                         <div className='card'>
-                            <Typography variant="h5">Greetings Vocabulary</Typography>
+                            <Typography variant="h5">Common Home Nouns</Typography>
+                            <ConjugationTable data={Nouns} />
+                        </div>
+                    </Paper>
+
+                    <Paper elevation={7} sx={{ backgroundColor: "#E8E9EB", width: 3 / 4, marginTop: '50px' }} >
+                        <div className='card'>
+                            <Typography variant="h5">Common Home Verbs</Typography>
+                            <ConjugationTable data={Verbs} />
+                        </div>
+                    </Paper>
+
+                    <Paper elevation={7} sx={{ backgroundColor: "#E8E9EB", width: 3 / 4, marginTop: '50px' }} >
+                        <div className='card'>
+                            <Typography variant="h5">Common Home Adjectives</Typography>
+                            <ConjugationTable data={Adjectives} />
+                        </div>
+                    </Paper>
+
+
+                    {/* Flashcards*/}
+                    <Paper elevation={7} sx={{ backgroundColor: "#E8E9EB", width: 1 / 2, marginTop: '50px' }} >
+                        <div className='card' >
+                            <Typography variant="h5">Study Home</Typography>
+                            {/* <FlashcardArray cycle={true} cards={cards} /> */}
                         </div>
                     </Paper>
 
