a3d72133424e238d7f484380ca7032595a62c59e
yusufshhzd <yusufshhzd@gmail.com>
2025-06-16 14:14:01 -0400

    updated images

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -188,7 +188,7 @@ const data = {
     },
   ],
   projects: [
-   
+
   ],
 }
 
@@ -201,7 +201,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
             <SidebarMenuButton size="lg" asChild>
               <a href="#">
                 <div className="flex aspect-square size-8 items-center justify-center rounded-lg bg-sidebar-primary text-sidebar-primary-foreground">
-                  <Command className="size-4" />
+                  <img src="/logo.svg" alt="Seekho" className="size-6 mt-2" />
                 </div>
                 <div className="grid flex-1 text-left text-sm leading-tight">
                   <span className="truncate font-semibold">Learn Urdu</span>
