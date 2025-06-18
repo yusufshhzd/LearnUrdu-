36a6a1546e7dbc6c3c530997cef307958e9eb21c
yusufshhzd <yusufshhzd@gmail.com>
2025-06-18 18:54:19 -0400

    introduction page

--- src/components/app-sidebar.tsx
+++ src/components/app-sidebar.tsx
@@ -214,7 +214,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
       </SidebarHeader>
       <SidebarContent>
         <NavMain items={data.navMain} />
-        <NavProjects projects={data.projects} />
+        {/* <NavProjects projects={data.projects} /> */}
         <NavSecondary items={data.navSecondary} className="mt-auto" />
       </SidebarContent>
       <SidebarFooter>
--- src/pages/Introduction/Introduction.tsx
+++ src/pages/Introduction/Introduction.tsx
@@ -1,14 +1,35 @@
-import React from 'react'
-import ProgressBar from '@/components/ProgressBar/ProgressBar';
+import React from 'react';
+import '/src/css/WelcomePage.css';
+import { SidebarProvider, SidebarTrigger } from '@/components/ui/sidebar';
+import { AppSidebar } from '@/components/app-sidebar';
+import { useNavigate } from 'react-router-dom';
 
 
-const Introduction = () => {
+const WelcomePage = () => {
+  
+  const navigate = useNavigate();
+
   return (
-    <div style={{ height: '200vh' }}>
-      <ProgressBar />
-      Welcome
-    </div>
-  )
-}
+    <div>
 
-export default Introduction
+      <SidebarProvider>
+        <AppSidebar />
+        <SidebarTrigger />
+        <div className="rightOfSidebar">
+          <div className="welcome-container">
+            <div className="welcome-card">
+              <h1>Welcome</h1>
+              <p className="subtitle">Learn Urdu easily through the Roman script!</p>
+              <button className = "start-button" onClick={() => navigate('/hona')} >Start Learning </button>
+
+            </div>
+          </div>
+        </div>
+        </SidebarProvider>
+
+    </div>
+
+  );
+};
+
+export default WelcomePage;
